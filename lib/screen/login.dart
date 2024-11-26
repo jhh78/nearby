78f15eb6e248c8427886b194c2c347ab9558f740
鄭會憲 <vkcel400@gmail.com>
2024-11-27 07:14:16 +0900

    로그인 UI

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,26 +1,73 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
 import 'package:nearby/screen/policy.dart';
+import 'package:nearby/widget/login/login_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  void handleLoginGoogleLogin() {
+    log("Google Login");
+  }
+
+  void handleAppleLogin() {
+    log("Apple Login");
+  }
+
+  void handleEmailLogin() {
+    log("Email Login");
+  }
+
+  void handleRegister() => Get.offAll(() => PolicyScreen(), transition: Transition.fade);
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => {Get.to(() => PolicyScreen())},
-      child: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Image.asset(
-            "images/login.png",
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Image.asset(
+              "images/login.png",
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                LoginButton(
+                  icon: Icons.g_mobiledata,
+                  text: "Googleアカウントでログイン",
+                  callback: handleLoginGoogleLogin,
+                ),
+                const SizedBox(height: 10),
+                LoginButton(
+                  icon: Icons.apple_outlined,
+                  text: "Appleアカウントでログイン",
+                  callback: handleAppleLogin,
+                ),
+                const SizedBox(height: 10),
+                LoginButton(
+                  text: "Emailでログイン",
+                  callback: handleEmailLogin,
+                ),
+                const SizedBox(height: 10),
+                LoginButton(
+                  text: "新規登録",
+                  callback: handleRegister,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widget/login/login_button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    super.key,
+    required this.callback,
+    required this.text,
+    this.icon,
+  });
+
+  final Function callback;
+  final String text;
+  final IconData? icon;
+
+  Size getButtonWidthSize(BuildContext context) {
+    return Size(MediaQuery.of(context).size.width * 0.85, 50);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: getButtonWidthSize(context),
+        maximumSize: getButtonWidthSize(context),
+        backgroundColor: Colors.white,
+        shadowColor: Colors.grey, // 그림자 컬러 적용
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+          side: const BorderSide(
+            color: Colors.black54,
+            width: 2,
+            style: BorderStyle.solid,
+          ),
+        ),
+      ),
+      onPressed: () => callback(),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (icon != null)
+            Icon(
+              icon,
+              color: Colors.black,
+              size: 28,
+            ),
+          const SizedBox(width: 10),
+          Text(
+            text,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ],
+      ),
+    );
+  }
+}
